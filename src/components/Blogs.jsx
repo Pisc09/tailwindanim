aec83df80ad7a5039b0787672ed93da2eb310667
pisco <pisco8118@gmail.com>
2024-07-07 00:38:53 +0200

    mise en place d'animation

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,3 +1,5 @@
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
+
 const posts = [
   {
     id: 1,
@@ -49,6 +51,22 @@ const posts = [
   },
   {
     id: 4,
+    title: "Boost your conversion rate",
+    href: "#",
+    description:
+      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
+    imageUrl:
+      "https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80",
+    date: "Mar 16, 2020",
+    datetime: "2020-03-16",
+    author: {
+      name: "Michael Foster",
+      imageUrl:
+        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    id: 5,
     title: "Boost your conversion rate",
     href: "#",
     description:
@@ -121,6 +139,20 @@ export default function Blogs() {
               </h3>
             </article>
           ))}
+        </div>
+      </div>
+      <div className="flex mt-28 justify-center">
+        <div className="relative flex items-center gap-x-4 rounded-full px-4 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+          <span className="font-semibold text-indigo-600">We’re hiring</span>
+          <span aria-hidden="true" className="h-4 w-px bg-gray-900/10" />
+          <a href="#" className="flex items-center gap-x-1 group">
+            <span aria-hidden="true" className="absolute inset-0" />
+            See open positions
+            <ChevronRightIcon
+              aria-hidden="true"
+              className="-mr-2 h-5 w-5 text-gray-400 group-hover:translate-x-1 transition-transform ease-in-out"
+            />
+          </a>
         </div>
       </div>
     </div>
